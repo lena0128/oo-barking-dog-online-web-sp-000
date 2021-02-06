75921c32ab1718d98d5c4771306624184e44836a
Lena Hou <lenahou2013@gmail.com>
2021-02-06 23:11:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Dog
+  def name=(dogs_name)
+    @this_dog_name = dogs_name
+  end
+
+  def name
+    @this_dog_name
+  end
+
+  def bark
+    puts "woof!"
+  end
+end
